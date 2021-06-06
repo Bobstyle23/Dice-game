39d0c04199999a46eb14f21449a94cab22467d54
Bob Pakhriev <boburpakhriev@gmail.com>
2021-06-06 14:23:06 +0900

    Add refresh button

--- index.js
+++ index.js
@@ -22,3 +22,7 @@ if (randomNumber1 > randomNumber2) {
 } else if (randomNumber1 === randomNumber2) {
   document.querySelector("h1").innerHTML = "Draw! ";
 }
+
+function refreshPage() {
+  window.location.reload();
+}
