ae505fd163462406067479b6c93c26c4cf7f26bc
Bob Pakhriev <boburpakhriev@gmail.com>
2021-06-08 09:54:13 +0900

    Finally change to jquery

--- index.js
+++ index.js
@@ -9,23 +9,25 @@ let scores = [0, 0];
 let roundScore = 0;
 let active = 0;
 
-let image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomImageSource);
+let image1 = $("img")[0];
+$(image1).attr("src", randomImageSource);
+// image1.setAttribute("src", randomImageSource);
 
 // Dice 2
 let randomNumber2 = Math.floor(Math.random() * 6 + 1);
 let randomDiceImage2 = "dice" + randomNumber2 + ".png";
 let randomImageSource2 = "images/" + randomDiceImage2;
 
-let image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomImageSource2);
+let image2 = $("img")[1];
+$(image2).attr("src", randomImageSource2);
+// image2.setAttribute("src", randomImageSource2);
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = " 🚩Player 1  Wins!";
+  $("h1").html(" 🚩Player 1  Wins!");
 } else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = " Player 2 Wins! 🚩";
+  $("h1").html(" Player 2 Wins! 🚩");
 } else if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerHTML = "Draw! ";
+  $("h1").html("Draw! ");
 }
 
 function refreshPage() {
@@ -36,7 +38,7 @@ function refreshPage() {
 function countScoreForPlayer1() {
   let score = 0;
   if (document.getElementById("title") === " 🚩Player 1  Wins!") {
-    document.getElementById("playerOne") += 1;
+    document.getElementById("playerOne")[score] += 1;
   }
 }
 
